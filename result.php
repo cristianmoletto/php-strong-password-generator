26d95c7f7b081220588f965ee372fe7fef585e89
cristianmoletto <cristian.moletto@gmail.com>
2026-04-25 11:55:38 +0200

    feat: milestone 3

--- result.php
+++ result.php
@@ -1,15 +1,23 @@
 <?php
 
+// gestione sessione
+session_start();
+$password = $_SESSION['password'];  // accede al parametro passw
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Risultato password</title>
 </head>
+
 <body>
-    <h1>La tua password di X caratteri</h1>
+    <nav><a href="./index.php">Torna indietro</a></nav>
+    <h1>La tua password di <?php echo strlen($password) ?> caratteri</h1>
+    <pre><?php echo $password ?></pre>
 </body>
+
 </html>
